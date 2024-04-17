81fa61a19f672c37a132a185e31201d20bcce45c
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2024-04-17 09:22:45 +0200

    Added Pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-alert('Hello world');
-
-let favoriteFood = "apple"
-
-document.write(favoriteFood)
+pokemonList = [
+    {name: "Charizard", height: 1.7, types: ["fire", "flying"], abilities: ["blaze", "solar-power"]},
+    {name: "Arbok", height: 3.5, types: ["poison"], abilities: ["intimidate", "shed-skin", "unnerve"]},
+    {name: "Ninetales", height: 1.1, types: ["fire"], abilities: ["flash-fire", "drought"]},
+    {name: "Magneton", height: 1, types: ["electric", "steel"], abilities: ["sturdy", "magnet-pull", "analytic"]},
+    {name: "Reshiram", height: 3.2, types: ["dragon", "fire"], abilities: ["turboblaze"]},
+]
